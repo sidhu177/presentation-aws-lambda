d5e72cb0f632b38be8d29b90a988c2072ba9ea11
Sid Ramesh <sidharth177@gmail.com>
2018-09-05 00:27:34 -0400

    added environmental variables

--- simple_print.py
+++ simple_print.py
@@ -1,9 +1,13 @@
 #simple print, also configure the event
 
 import json
+import os
+
+# val =  os.environ['Key1']
 
 def lambda_handler(event,context):
     
     print('message to be printed '+ event['message'])
+    # print(val)
     return event['message']
     
