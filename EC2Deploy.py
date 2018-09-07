02ffd708aa0dbb50a9acb185d4b58beed25c9e8a
sidhu177 <sidharth177@gmail.com>
2018-09-07 03:18:51 -0400

    made some changes

--- EC2Deploy.py
+++ EC2Deploy.py
@@ -1,14 +1,12 @@
 import boto3
 
-# Defining the EC2
 REGION = 'us-east-1' # region to launch instance.
 AMI = 'ami-6871a115'  # Red Hat Image
 INSTANCE_TYPE = 't2.micro' # instance type to launch.
 SG = 'sg-02e28977'  # Default Security Group
-EC2Key = 'AWSKeyPair' # Key Pair
+EC2Key = 'AWSKeyPair' # Kay Pair
 EC2 = boto3.client('ec2', region_name=REGION)
 
-# installing an Apache Server
 init_script = """#!/bin/bash
 sudo yum update -y
 sudo yum install httpd -y
@@ -16,26 +14,25 @@ sudo systemctl start httpd.service
 """
 
 def lambda_handler(event, context):
-
- 
-def lambda_handler(event, context):
     
-    state = event['detail']['state']
-    
-    if state == ['terminated']:
+    state = event["detail"]["state"]
+    print(state)
+    if state==["terminated"] or state=='terminated':
        instance = EC2.run_instances(
-           ImageId=AMI,
-           InstanceType=INSTANCE_TYPE,
-           MinCount=1, # required by boto, even though it's kinda obvious.
-           MaxCount=1,
-           InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
-           KeyName = EC2Key,
-           SecurityGroupIds = [SG],
-           UserData=init_script 
-        )
-
+       ImageId=AMI,
+       InstanceType=INSTANCE_TYPE,
+       MinCount=1, 
+       MaxCount=1,
+       InstanceInitiatedShutdownBehavior='terminate', 
+       KeyName = EC2Key,
+       SecurityGroupIds = [SG],
+       UserData=init_script )
+    else:
+       print("its all good!")
+       
     print("New instance created.")
     instance_id = instance['Instances'][0]['InstanceId']
     print(instance_id)
-
     return instance_id
+    
+    
