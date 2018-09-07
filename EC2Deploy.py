8a3487aa4fd82a03aac2402cf456fb4e994ec7f3
sidhu177 <sidharth177@gmail.com>
2018-09-07 01:19:18 -0400

    made some changes

--- EC2Deploy.py
+++ EC2Deploy.py
@@ -1,20 +1,35 @@
 import boto3
 
-REGION = 'us-east-1' # region 
-AMI = 'ami-6871a115' # RED HAT AMI
-INSTANCE_TYPE = 't2.micro' # instance type
+# Defining the EC2
+REGION = 'us-east-1' # region to launch instance.
+AMI = 'ami-6871a115'  # Red Hat Image
+INSTANCE_TYPE = 't2.micro' # instance type to launch.
+SG = 'sg-02e28977'  # Default Security Group
+EC2Key = 'AWSKeyPair' # Key Pair
 EC2 = boto3.client('ec2', region_name=REGION)
 
+# installing an Apache Server
+init_script = """#!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd.service
+"""
+
 def lambda_handler(event, context):
+
     instance = EC2.run_instances(
-        ImageId=AMI,
-        InstanceType=INSTANCE_TYPE,
-        MinCount=1, # required by boto, even though it's kinda obvious.
-        MaxCount=1,
-        InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
+        ImageId=AMI, # Passing Red Hat image
+        InstanceType=INSTANCE_TYPE, # Passing t2 Micro
+        MinCount=1, # required by boto
+        MaxCount=1, # required by boto
+        InstanceInitiatedShutdownBehavior='terminate', # Shutdown Behaviour
+        KeyName = EC2Key, # Key Pair
+        SecurityGroupIds = [SG], # Default Security Group
+        UserData=init_script  # running the install commands
     )
 
     print("New instance created.")
     instance_id = instance['Instances'][0]['InstanceId']
     print(instance_id)
+
     return instance_id
