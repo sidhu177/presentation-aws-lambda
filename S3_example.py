2965e05d9879333e36cd4243ddc0356cacc1cfbb
sidhu177 <sidharth177@gmail.com>
2018-09-08 03:14:54 -0400

    S3 to S3 and SNS

--- S3_example.py
+++ S3_example.py
@@ -10,15 +10,26 @@ def lambda_handler(event, context):
     print(bucket)
     backup = 'novalugduplicatebucket'
     print(backup)
-    key = event['Records'][0]['s3']['object']['key']
-    print(key)
-    #print(type(key))
-    backup_response = s3.copy_object(Key=key, Bucket=backup, CopySource={"Bucket":bucket, "Key":key})
+    object = event['Records'][0]['s3']['object']['key']
+    print(object)
+    #print(type(object))
+    backup_response = s3.copy_object(Bucket=backup,Key=object, CopySource={"Bucket":bucket, "Key":object})
     print(backup_response)
     state = backup_response["ResponseMetadata"]["HTTPStatusCode"] 
+    print(state)
+    
+    sns = boto3.client('sns')
+    sns_resource = 'arn:aws:sns:us-east-1:929003018535:novalugs3tosns'
+    sns.publish(
+        TopicArn = sns_resource,
+        Subject = 'New Object in your Bucket!',
+        Message=('New object has been uploaded to your S3: '+ str(object))
+        #PhoneNumber='+123456789'
+)
     if state == 200:
-        return "Successfully duplicated %s to %s" % (key, backup)
+        return "Successfully duplicated %s to %s" % (object, backup)
     else:
         return "Unable to duplicate object"
-    
+
+   
     return "The object should be duplicated"
